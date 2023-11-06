f54c0140197ef9bca3050ad791174c8de7fc0b8c
Mike Reiche <mike.reiche@ewerk.com>
2023-11-06 11:54:43 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description="Yet another python streams library",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.1.5",
+    version="0.1.6",
     url="https://github.com/mreiche/python-streams",
     author="Mike Reiche",
     py_modules=['tinystream'],
